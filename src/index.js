5f48b993069dfb4c63c9202840fe221aeebc0669
liuyao <reactjser@163.com>
2019-11-09 16:22:50 +0800

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-// If you need some help, you can concact me by wechat: landiaoshenghuo
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
